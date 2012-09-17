6d7a99894b1ec5fb408eff0342701d2f115389b9
recyclebin5385 <recyclebin5385@yahoo.co.jp>
2012-09-17 16:37:16 +0900

    first commit

---
+++ pixiv_random_search.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name        pixiv random search
+// @namespace   pixivrandomsearch
+// @description a Greasemonkey script to add random search button to pixiv search.php and tags.php
+// @include     http://www.pixiv.net/tags.php*
+// @include     http://www.pixiv.net/search.php*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+
+
+$(function() {
+    $("nav.pager ul").append("<li><a href='#' class='ui-button-light' title='random'>random</a></li>").click(function(){
+        var maxPage = Math.floor((parseInt($("#page-search .info .count").text()) + 19) / 20);
+
+        var page = Math.floor(Math.random() * maxPage) + 1;
+
+        var newUrl;
+        var currentUrl = document.URL;
+        var regExp = new RegExp("(p=[0-9]*)");
+        if (currentUrl.match(/p=[0-9]*/)) {
+            newUrl = currentUrl.replace(/p=[0-9]*/, "p=" + page);
+        } else {
+            if (currentUrl.match(/\?/)) {
+                newUrl = currentUrl + "&p=" + page;
+            } else {
+                newUrl = currentUrl + "?p=" + page;
+            }
+        }
+
+        location.href = newUrl;
+    })});
