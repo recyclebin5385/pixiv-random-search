041382978153b2533e31a433a77b1299c975a384
recyclebin5385 <recyclebin5385@yahoo.co.jp>
2013-01-31 23:09:13 +0900

    pixivの仕様変更にあわせる

--- pixiv_random_search.user.js
+++ pixiv_random_search.user.js
@@ -4,16 +4,16 @@
 // @description a Greasemonkey script to add random search button to pixiv search.php and tags.php
 // @include     http://www.pixiv.net/tags.php*
 // @include     http://www.pixiv.net/search.php*
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
 
 
 
 $(function() {
-    $("nav.pager ul").append("<li><a href='#' class='ui-button-light' title='random'>random</a></li>").click(function(){
-        var maxPage = Math.min(Math.floor((parseInt($("#page-search .info .count").text()) + 19) / 20), 1000);
-
+    $("ul.pager-items").append("<li><a href='#' class='ui-button-light' title='random'>random</a></li>").click(function(){
+        var maxPage = Math.min(Math.floor((parseInt($(".column-title-container .count-badge").text()) + 19) / 20), 1000);
+        alert((parseInt($(".column-title-container .count-badge").text())));
         var page = Math.floor(Math.random() * maxPage) + 1;
 
         var newUrl;
