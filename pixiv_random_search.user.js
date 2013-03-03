c79f6b938d68e005353c4512db34c68c71618205
recyclebin5385 <recyclebin5385@yahoo.co.jp>
2013-03-03 16:31:08 +0900

    pixivの仕様変更に対応する

--- pixiv_random_search.user.js
+++ pixiv_random_search.user.js
@@ -1,32 +1,34 @@
 // ==UserScript==
 // @name        pixiv random search
 // @namespace   pixivrandomsearch
-// @description a Greasemonkey script to add random search button to pixiv search.php and tags.php
-// @include     http://www.pixiv.net/tags.php*
-// @include     http://www.pixiv.net/search.php*
-// @version     2
+// @description a Greasemonkey script to add random search button to pixiv pages with a pager
+// @include     http://www.pixiv.net/*
+// @version     3
 // @grant       none
 // ==/UserScript==
 
 
-
 $(function() {
-    $("ul.pager-items").append("<li><a href='#' class='ui-button-light' title='random'>random</a></li>").click(function(){
-        var maxPage = Math.min(Math.floor((parseInt($(".column-title-container .count-badge").text()) + 19) / 20), 1000);
+    $(".pager-container .next").after($("<a href='#' style='line-height: 22px;' class='_button' title='random'>random</a>").click(function(){
+        var countText = $("#wrapper .count-badge").text();
+        if (!countText.match(/([0-9]+)/)) {
+            return;
+        }
+        var maxPage = Math.min(Math.floor((parseInt(RegExp.$1) + 19) / 20), 1000);
+        alert(maxPage);
         var page = Math.floor(Math.random() * maxPage) + 1;
 
         var newUrl;
         var currentUrl = document.URL;
         var regExp = new RegExp("(p=[0-9]*)");
         if (currentUrl.match(/p=[0-9]*/)) {
-            newUrl = currentUrl.replace(/p=[0-9]*/, "p=" + page);
+            newUrl = currentUrl.replace(/p=[0-9a-zA-Z]*/, "p=" + page);
         } else {
             if (currentUrl.match(/\?/)) {
-                newUrl = currentUrl + "&p=" + page;
+                newUrl = currentUrl.replace(/#$/, '') + "&p=" + page;
             } else {
-                newUrl = currentUrl + "?p=" + page;
+                newUrl = currentUrl.replace(/#$/, '') + "?p=" + page;
             }
         }
-
         location.href = newUrl;
-    })});
+    }))});
